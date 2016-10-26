f1b4d38e366beed95849f1025c64dea48e796d7e
BrandonZamorano <730242@rqs.c2>
2016-10-26 10:52:33 -0500

    Update files with edits
    Updated code inside.

--- DATE.h
+++ DATE.h
@@ -1,10 +1,11 @@
 /*
-Name:			Brandon Zamorano
-Date:			10.24.2016
-Description:	Date class definition; Member fuctions define in Date.cpp
+Brandon Zamorano
+Today
+Date class definiton; member functions defined in date.cpp
 */
-#ifndef  DATE_H
-#define  DATE_H
+
+#ifndef DATE_H
+#define DATE_H 
 
 #include <string>
 using namespace std;
@@ -12,35 +13,39 @@ using namespace std;
 class Date
 {
 	public:
-		Date(); // default constructor uses <ctime> fuctions to set date
-		Date(int, int); // constructor using ddd yyyy format
-		Date(int, int, int); //constructor using dd/mm/yy format
-		Date(string, int, int); //constructor for using Month dd, yyy format
-		void setDay(int); //set the day
-		void setMonth(int); //set the month
+		Date();//default constructor uses <ctime>
+		Date(int, int); //constructor
+		Date(int, int, int); //constructor using dd/mm/yy
+		Date(string, int, int); //constructor for using month dd, yyyy, format
+		void setDay(int); //sets the day
+		void setMonth(int);//set the month
 		void print() const; //print date in the month/day/year format
-		void printDDDYYYY() const; //print date in ddd yyyy format
-		void printMMDDYY() const;//print date in mm/dd/yy format
-		void printMonthDDYYYY() const; //print date in Month, dd, yyyy format
-		~Date();//destruction
+		void printDDDYYYY()const; //print date in ddd yyyy dormat
+		void printMMDDYY() const; //pring date in month mm.dd,yyyy format
+		void printMonthDDYYYY() const; //pprint date in month dd,yyyy format
+		~Date();
 
 	private:
-		int month; //1-12 (January-December)
+		int month;// 1-12 (January- December)
 		int day; //1-31 based on month
-		int year; //doesnt matter
+		int year; //year
 
-		//utility fuctions'
-		int checkDay(int) const;//check if day is proper for the month and year
-		int daysInMonth(int) const; //returns number of days in given month
-		bool isLeapYear() const; //indicates whether date is in a leap year
-		int convertDDToDDD() const; //get 3-digit day based on month and a day
-		void setMMDDFromDDD(int); //set 3-digit days based on month and day
-		string convertMMtoMonth(int) const; //
-		void setMMFromMonth(string); //convert month name to mm
-		int convertYYYYtoYY() const; //get 2 digit year based on 4 diget year
-		void setYYYYFromYY(int); //set year based on 2 digit year
+		//utility functions
+		int checkDay(int) const; //check if day is propter for the month and year
+		int daysInMonth(int)const; //returns number of days in given month
+		bool isLeapYear() const; //indicates whether date is in leap year 
+		int convertDDToDDD(int); //get 3 digit date based on month and day
+		void setMMDDFromDDD(int); //set 3 didgit days based on month and day
+		string convertMMToMonth(int) const; //get 2 digit year based on 4 digit year
+		void setMMFromMonth(string); //conver month name to MM
+		int convertYYYYToYY() const; //gets 2 digit year based on 4 digit year
+		void setYYYFromYY(int); //set year based on 2 digit year
 
-
+		
 };
 
 #endif
+
+
+
+
